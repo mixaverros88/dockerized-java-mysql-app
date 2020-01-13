b69432944807d0aacbc96bab4f251041520a7115
Mike verros <mixalisverros@hotmail.gr>
2020-01-13 16:48:51 +0200

    upload mysql dockerfile and relative files

---
+++ mysql/run.sh
@@ -0,0 +1,1 @@
+docker run -d -p 3306:3306 --name my-mysql -e MYSQL_ROOT_PASSWORD=supersecret my-mysql
---
+++ mysql/sql-scripts/InsertData.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (first_name, last_name, department, email) 
+VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com');
