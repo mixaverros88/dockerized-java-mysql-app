647d89969a36e7fbf54b21c18d38ac543f6a8f71
Mike verros <mixalisverros@hotmail.gr>
2020-01-22 12:10:14 +0200

    minor updates

--- java/run.sh
+++ java/run.sh
@@ -5,7 +5,7 @@ cd deploy
 # Download the war from gihub
 curl -LOk https://github.com/mixaverros88/docker_with_java_and_mysql/raw/master/java/target/RESTfulExample.war
 # run the docker
-winpty docker run -it -p 80:8080 -v /$(pwd):/usr/local/tomcat/webapps java-connect-with-mysql
+winpty docker run  -d --name javaapp -p 80:8080 -v /$(pwd):/usr/local/tomcat/webapps java-connect-with-mysql
 # you can reach this service through docker machine ip, also add the demorest folder to the path
 # to find docker-machine ip execute the following command
 # docker-machine ls
