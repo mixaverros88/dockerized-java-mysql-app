89cf125fae7bb0d2a3ad3addef464fb1718e3674
Mike verros <mixalisverros@hotmail.gr>
2020-01-22 11:11:48 +0200

    update java compile version

--- java/run.sh
+++ java/run.sh
@@ -1,1 +1,12 @@
-docker run -d --name javaapp java-connect-with-mysql
+filepath=$PWD
+cd $filepath
+mkdir deploy
+cd deploy
+# Download the war from gihub
+curl -LOk https://github.com/mixaverros88/docker_with_java_and_mysql/raw/master/java/target/RESTfulExample.war
+# run the docker
+winpty docker run -it -p 80:8080 -v /$(pwd):/usr/local/tomcat/webapps java-connect-with-mysql
+# you can reach this service through docker machine ip, also add the demorest folder to the path
+# to find docker-machine ip execute the following command
+# docker-machine ls
+# example URL http://192.168.99.100/demorest/
--- network/create_network.sh
+++ network/create_network.sh
@@ -1,3 +1,4 @@
+# create an network in order to enable communication between containers throu out container name
 docker network create --driver bridge isolated 
 docker network connect isolated mysqlapp
 docker network connect isolated javaapp
